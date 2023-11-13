e6aa6a354a92f31ece0160386943c22fad8b4dc9
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-11-13 17:19:31 -0300

    introducción a dart

---
+++ 01_dart_introduccion/01-hello-world.dart
@@ -0,0 +1,10 @@
+void main() {
+  //   var myName = 'Nicolás';
+  //   late final myName;
+  //   String myName = 'Nicolás';
+  const myName = 'Nicolás';
+    
+  print('Hola $myName');
+  print('Hola ${ myName.toUpperCase() }');
+  print('Hola ${ 1 + 1 }');
+}
