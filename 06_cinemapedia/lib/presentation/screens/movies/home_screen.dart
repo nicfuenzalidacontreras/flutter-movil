9b676b57396e3dff862a5a8c4c9e2c051a729afc
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-11-30 08:52:51 -0300

    full app cinemapedia parte uno

---
+++ 06_cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:cinemapedia/config/constants/environment.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const name = 'home-scress';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text(Environment.theMovieDbKey)));
+  }
+}
