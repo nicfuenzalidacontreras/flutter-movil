8898515f66208a5db5f5a80a56cbb9c52f78606a
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-12-01 10:33:20 -0300

    full app cinemapedia parte dos

--- 06_cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ 06_cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -8,6 +8,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text(Environment.theMovieDbKey)));
+    return const Scaffold(body: Placeholder());
   }
 }
