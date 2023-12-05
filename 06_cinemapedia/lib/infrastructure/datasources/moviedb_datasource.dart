7126c03b4979eda75b4e16d879893f779cb67d0a
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-12-05 17:06:41 -0300

    cinemapedia continuacion parte uno

--- 06_cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
+++ 06_cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
@@ -15,7 +15,7 @@ class MoviedbDatasource extends MoviesDataSource {
 
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-    final response = await dio.get('/movies/now_playing');
+    final response = await dio.get('/movie/now_playing');
     final movieDBResponse = MovieDbResponse.fromJson(response.data);
     final List<Movie> movies = movieDBResponse.results
         .where((movie) => movie.posterPath != 'no-poster')
---
+++ 06_cinemapedia/lib/presentation/widgets/widgets.dart
@@ -0,0 +1,2 @@
+export "package:cinemapedia/presentation/widgets/shared/custom_appbar.dart";
+export "package:cinemapedia/presentation/widgets/movies/movies_slideshow.dart";
