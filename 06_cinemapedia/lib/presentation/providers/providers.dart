8bf0ccca4d6ae60f4ec97f43f1523781fa9a5b2f
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-12-12 16:27:33 -0300

    películas individuales y actores parte dos

--- 06_cinemapedia/lib/presentation/providers/providers.dart
+++ 06_cinemapedia/lib/presentation/providers/providers.dart
@@ -2,3 +2,4 @@ export 'movies/movies_providers.dart';
 export 'movies/movies_repository_provider.dart';
 export 'movies/movies_slideshow_provider.dart';
 export 'movies/inicial_loading_provider.dart';
+export 'movies/movie_info_provider.dart';
