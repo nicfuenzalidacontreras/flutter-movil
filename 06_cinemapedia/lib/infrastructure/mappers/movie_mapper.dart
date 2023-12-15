0f59da135262b84381fbce8a7ad220b90101a58c
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-12-15 11:03:11 -0300

    searchDelegate - busquedas parte dos

--- 06_cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
+++ 06_cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
@@ -3,43 +3,44 @@ import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
-  static Movie movieDBToEntity(MovieMovieDB movieDB) => Movie(
-      adult: movieDB.adult,
-      backdropPath: (movieDB.backdropPath != '')
-          ? 'https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}'
-          : 'https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
-      genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
-      id: movieDB.id,
-      originalLanguage: movieDB.originalLanguage,
-      originalTitle: movieDB.originalTitle,
-      overview: movieDB.overview,
-      popularity: movieDB.popularity,
-      posterPath: (movieDB.posterPath != '')
-          ? 'https://image.tmdb.org/t/p/w500/${movieDB.posterPath}'
-          : 'no-poster',
-      releaseDate: movieDB.releaseDate,
-      title: movieDB.title,
-      video: movieDB.video,
-      voteAverage: movieDB.voteAverage,
-      voteCount: movieDB.voteCount);
+  static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
+      adult: moviedb.adult,
+      backdropPath: (moviedb.backdropPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+          : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
+      genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+      id: moviedb.id,
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle: moviedb.originalTitle,
+      overview: moviedb.overview,
+      popularity: moviedb.popularity,
+      posterPath: (moviedb.posterPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+          : 'https://www.movienewz.com/img/films/poster-holder.jpg',
+      releaseDate:
+          moviedb.releaseDate != null ? moviedb.releaseDate! : DateTime.now(),
+      title: moviedb.title,
+      video: moviedb.video,
+      voteAverage: moviedb.voteAverage,
+      voteCount: moviedb.voteCount);
 
-  static Movie movieDetailsToEntity(MovieDetails movieDB) => Movie(
-      adult: movieDB.adult,
-      backdropPath: (movieDB.backdropPath != '')
-          ? 'https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}'
-          : 'https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
-      genreIds: movieDB.genres.map((e) => e.name).toList(),
-      id: movieDB.id,
-      originalLanguage: movieDB.originalLanguage,
-      originalTitle: movieDB.originalTitle,
-      overview: movieDB.overview,
-      popularity: movieDB.popularity,
-      posterPath: (movieDB.posterPath != '')
-          ? 'https://image.tmdb.org/t/p/w500/${movieDB.posterPath}'
-          : 'https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
-      releaseDate: movieDB.releaseDate,
-      title: movieDB.title,
-      video: movieDB.video,
-      voteAverage: movieDB.voteAverage,
-      voteCount: movieDB.voteCount);
+  static Movie movieDetailsToEntity(MovieDetails moviedb) => Movie(
+      adult: moviedb.adult,
+      backdropPath: (moviedb.backdropPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+          : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
+      genreIds: moviedb.genres.map((e) => e.name).toList(),
+      id: moviedb.id,
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle: moviedb.originalTitle,
+      overview: moviedb.overview,
+      popularity: moviedb.popularity,
+      posterPath: (moviedb.posterPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+          : 'https://sd.keepcalms.com/i-w600/keep-calm-poster-not-found.jpg',
+      releaseDate: moviedb.releaseDate,
+      title: moviedb.title,
+      video: moviedb.video,
+      voteAverage: moviedb.voteAverage,
+      voteCount: moviedb.voteCount);
 }
--- 06_cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
+++ 06_cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +8,18 @@ typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
 
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
   final SearchMoviesCallback searchMovies;
+  StreamController<List<Movie>> debounceMovies = StreamController.broadcast();
+  Timer? _debounceTimer;
 
   SearchMovieDelegate({required this.searchMovies});
+
+  void _onQueryChange(String query) {
+    if (_debounceTimer?.isActive ?? false) _debounceTimer!.cancel();
+
+    _debounceTimer = Timer(const Duration(milliseconds: 500), () {
+      //TODO: buscar película...
+    });
+  }
 
   @override
   String get searchFieldLabel => 'Buscar películas...';
@@ -36,18 +48,80 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return FutureBuilder(
-        future: searchMovies(query),
+    _onQueryChange(query);
+
+    return StreamBuilder(
+        stream: debounceMovies.stream,
         builder: ((context, snapshot) {
           final movies = snapshot.data ?? [];
           return ListView.builder(
               itemCount: movies.length,
-              itemBuilder: (context, index) {
-                final movie = movies[index];
-                return ListTile(
-                  title: Text(movie.title),
-                );
-              });
+              itemBuilder: (context, index) => _MovieItem(
+                    movie: movies[index],
+                    onMovieSelected: close,
+                  ));
         }));
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+  final Function onMovieSelected;
+
+  const _MovieItem(
+      {super.key, required this.movie, required this.onMovieSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return GestureDetector(
+      onTap: () {
+        onMovieSelected(context, movie);
+      },
+      child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          child: Row(children: [
+            SizedBox(
+              width: size.width * 0.2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(movie.posterPath,
+                    loadingBuilder: (context, child, loadingProgress) =>
+                        FadeIn(child: child)),
+              ),
+            ),
+            const SizedBox(width: 10),
+            SizedBox(
+              width: size.width * 0.7,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      movie.title,
+                      style: textStyle.titleMedium,
+                    ),
+                    (movie.overview.length > 100)
+                        ? Text('${movie.overview.substring(0, 100)}...')
+                        : Text(movie.overview),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star_half_rounded,
+                          color: Colors.yellow.shade800,
+                        ),
+                        const SizedBox(width: 5),
+                        Text(
+                          HumanFormats.number(movie.voteAverage, 1),
+                          style: textStyle.bodyMedium!
+                              .copyWith(color: Colors.yellow.shade900),
+                        ),
+                      ],
+                    )
+                  ]),
+            )
+          ])),
+    );
   }
 }
