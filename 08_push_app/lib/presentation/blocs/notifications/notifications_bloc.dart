980613a601479f56c0caabb2ded830c0b1e7802a
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2024-01-31 16:23:44 -0300

    notificaciones push parte tres

--- 08_push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
+++ 08_push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -11,15 +11,45 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
   NotificationsBloc() : super(const NotificationsState()) {
-    //on<NotificationsEvent>((event, emit) {
-    // TODO: implement event handler
-    //});
+    on<NotificationStatusChange>(_notificationStatusChange);
+    _initialStatusCheck();
+    _onForegroundMessage();
   }
 
   static Future<void> initializeFirabaseNotifications() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+  }
+
+  void _notificationStatusChange(
+      NotificationStatusChange event, Emitter<NotificationsState> emit) {
+    emit(state.copyWith(status: event.status));
+    _getFCMToken();
+  }
+
+  void _initialStatusCheck() async {
+    final settings = await messaging.getNotificationSettings();
+    add(NotificationStatusChange(settings.authorizationStatus));
+  }
+
+  void _getFCMToken() async {
+    if (state.status != AuthorizationStatus.authorized) return;
+    final token = await messaging.getToken();
+    print(token);
+  }
+
+  void _handleRemoteMessage(RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification == null) return;
+
+    print('Message also contained a notification: ${message.notification}');
+  }
+
+  void _onForegroundMessage() {
+    FirebaseMessaging.onMessage.listen(_handleRemoteMessage);
   }
 
   void requestPermission() async {
@@ -33,6 +63,6 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
       sound: true,
     );
 
-    settings.authorizationStatus;
+    add(NotificationStatusChange(settings.authorizationStatus));
   }
 }
--- 08_push_app/lib/presentation/blocs/notifications/notifications_event.dart
+++ 08_push_app/lib/presentation/blocs/notifications/notifications_event.dart
@@ -1,3 +1,11 @@
 part of 'notifications_bloc.dart';
 
-sealed class NotificationsEvent {}
+sealed class NotificationsEvent {
+  const NotificationsEvent();
+}
+
+class NotificationStatusChange extends NotificationsEvent {
+  final AuthorizationStatus status;
+
+  NotificationStatusChange(this.status);
+}
